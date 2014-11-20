1b1037336489de5811ab02d6505a04aa85a96f86
luis martinez <LuisMtzJr@luiss-MacBook-Air.local>
2014-11-19 23:13:36 -0600

    Section 3 almost complete Destroy not working

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -22,4 +22,32 @@ class ProductsController < ApplicationController
   def new
     @product = Product.new(:name =>'product name', :price => 69)
   end #new
+  def show
+    @product = Product.where(:id => params[:id]).first
+  end #show
+  def edit
+    @product = Product.where(:id => params[:id]).first
+  end #edit
+  def update
+    @product = Product.where(:id => params[:id]).first
+
+    respond_to do |format|
+      if @product.update_attributes(params[:product])
+        format.html { redirect_to @product, :notice => 'Product was successfully updated.' }
+        format.json { head :no_content }
+      else
+        format.html { render action: "edit" }
+        format.json { render json: @product.errors, :status => :unprocessable_entity }
+      end
+    end
+  end #update
+  def destroy
+    @product = Product.where(:id => params[:id]).first
+    @product.destroy
+
+    respond_to do |format|
+      format.html { redirect_to products_url }
+      format.json { head :no_content }
+    end
+  end
 end
